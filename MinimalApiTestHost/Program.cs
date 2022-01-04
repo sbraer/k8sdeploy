6a27b7cf7b3eff6b8f84b51663df304c310160c5
az <sbraer@gmail.com>
2022-01-04 21:21:01 +0100

    Fix code

--- MinimalApiTestHost/Program.cs
+++ MinimalApiTestHost/Program.cs
@@ -2,17 +2,5 @@ using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-app.MapGet("/", () => $"Hello, World! v1.0: {GetHostName()}");
+app.MapGet("/", () => $"Hello, World! v1.0: {Dns.GetHostName()}");
 app.Run("http://*:3000");
-
-static string GetHostName()
-{
-    try
-    {
-        return Dns.GetHostName();
-    }
-    catch(Exception ex)
-    {
-        return ex.Message;
-    }
-}
